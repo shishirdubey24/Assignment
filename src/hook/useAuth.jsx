93ec4ab84a64d69d42c129fbb093b880825772bf
shishirdubey24 <shishirdubey221@gmail.com>
2026-04-29 15:35:20 +0530

    renamed import

--- src/hook/useAuth.jsx
+++ src/hook/useAuth.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import AuthContext from "../context/AuthContext";
+import AuthContext from "../contenxt/AuthContext";
 const useAuth = () => useContext(AuthContext);
 
 export default useAuth;
